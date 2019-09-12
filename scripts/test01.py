1c5256820612acfaa929cc8dd074243e84c80e43
lisa <13167334857@163.com>
2019-09-12 11:33:27 +0800

    Initial commit

---
+++ scripts/test01.py
@@ -0,0 +1,11 @@
+class Test01:
+    def test001(self):
+        print("测试1被执行!")
+
+    def test002(self):
+        print("测试2被执行!")
+
+    def test003(self):
+        print("测试3被执行!")
+        # 断言失败
+        assert 0
